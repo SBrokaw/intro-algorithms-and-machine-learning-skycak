574b4cccc478d106a158cbc71a308f73ff80bc7f
sbrokaw <brok.sea@gmail.com>
2025-07-31 21:52:55 -0700

    more tests

--- ch1_hello_world/ch1_tests.py
+++ ch1_hello_world/ch1_tests.py
@@ -1,111 +1,147 @@
-from hello_world import check_if_symmetric, convert_to_numbers
+from hello_world import check_if_symmetric, convert_to_numbers, convert_to_chars, get_intersection
 
 tests = [
     {
         'function': check_if_symmetric,
-        'input': 'a'*1000 + 'b'*1000 + 'a'*1000,
+        'input': ['a'*1000 + 'b'*1000 + 'a'*1000],
         'output': True
     },
     {
         'function': check_if_symmetric,
-        'input': "A man, a plan, a canal, Panama",
+        'input': ["A man, a plan, a canal, Panama"],
         'output': False
     },
     {
         'function': check_if_symmetric,
-        'input': ' ',
+        'input': [' '],
         'output': True
     },
     {
         'function': check_if_symmetric,
-        'input': '!!',
+        'input': ['!!'],
         'output': True
     },
     {
         'function': check_if_symmetric,
-        'input': '',
+        'input': [''],
         'output': True
     },
     {
         'function': check_if_symmetric,
-        'input': 'NaN',
+        'input': ['NaN'],
         'output': True
     },
     {
         'function': check_if_symmetric,
-        'input': '1e1',
+        'input': ['1e1'],
         'output': True
     },
     {
         'function': check_if_symmetric,
-        'input': 'Infinity',
+        'input': ['Infinity'],
         'output': False
     },
     {
         'function': check_if_symmetric,
-        'input': '-42',
+        'input': ['-42'],
         'output': False
     },
     {
         'function': check_if_symmetric,
-        'input': 'hello',
+        'input': ['hello'],
         'output': False
     },
     {
         'function': check_if_symmetric,
-        'input': 'raceCAr',
+        'input': ['raceCAr'],
         'output': True
     },
     {
         'function': convert_to_numbers,
-        'input': 'NaN',
+        'input': ['NaN'],
+        'output': [78, 97, 78]
+    },
+    {
+        'function': convert_to_numbers,
+        'input': ['Infinity'],
+        'output': [73, 110, 102, 105, 110, 105, 116, 121]
+    },
+    {
+        'function': convert_to_numbers,
+        'input': ['12,345'],
+        'output': [49, 50, 44, 51, 52, 53]
+    },
+    {
+        'function': convert_to_numbers,
+        'input': ['!@#$%'],
+        'output': [33, 64, 35, 36, 37]
+    },
+    {
+        'function': convert_to_numbers,
+        'input': ['12345'],
         'output': [49, 50, 51, 52, 53]
     },
     {
         'function': convert_to_numbers,
-        'input': 'Infinity',
-        'output': [49, 50, 51, 52, 53]
-    },
-    {
-        'function': convert_to_numbers,
-        'input': '12,345',
-        'output': [49, 50, 51, 52, 53]
-    },
-    {
-        'function': convert_to_numbers,
-        'input': '!@#$%',
-        'output': [49, 50, 51, 52, 53]
-    },
-    {
-        'function': convert_to_numbers,
-        'input': '12345',
-        'output': [49, 50, 51, 52, 53]
-    },
-    {
-        'function': convert_to_numbers,
-        'input': '0x1A3F',
+        'input': ['0x1A3F'],
         'output': [48, 120, 49, 65, 51, 70]
+    },
+    {
+        'function': convert_to_chars,
+        'output': 'Infinity',
+        'input': [[73, 110, 102, 105, 110, 105, 116, 121]]
+    },
+    {
+        'function': convert_to_chars,
+        'output': '12,345',
+        'input': [[49, 50, 44, 51, 52, 53]]
+    },
+    {
+        'function': convert_to_chars,
+        'output': '!@#$%',
+        'input': [[33, 64, 35, 36, 37]]
+    },
+    {
+        'function': convert_to_chars,
+        'output': '12345',
+        'input': [[49, 50, 51, 52, 53]]
+    },
+    {
+        'function': convert_to_chars,
+        'output': '0x1A3F',
+        'input': [[48, 120, 49, 65, 51, 70]]
+    },
+    {
+        'function': get_intersection,
+        'input': ['0x1A3F', '0xffFF'],
+        'output': ['0', 'x', 'F']
     }
 ]
 
 successes = 0
 failures = 0
 
+print("=".center(50, '='))
+print("Chapter 1 Tests".center(50, '='))
+print("=".center(50, '='))
+
 for test in tests:
     function = test['function']
     trial = test['input']
     goal = test['output']
-    result = function(trial)
+    if isinstance(trial, (list, tuple)):
+        result = function(*trial)
+    else:
+        result = function(trial)
 
     if result == goal:  
         successes += 1
     else:
         failures += 1
         print(f'!FAIL! {function.__name__}({trial})')
-        print(f'!FAIL!   returned {result}, expected {goal}')
+        print(f'!FAIL!   returned {result}')
         print(f'!FAIL!   expected {goal}')
 
-print("Chapter 1 Tests".center(50, '-'))
 print(f'Testing complete with {successes} passes and {failures} failures')
 
 
