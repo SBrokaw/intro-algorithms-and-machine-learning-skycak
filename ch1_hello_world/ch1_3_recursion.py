253d87c78966ee0ce4700fa75f514721d8ab6123
sbrokaw <brok.sea@gmail.com>
2025-09-09 15:10:18 -0700

    finished recursion section

--- ch1_hello_world/ch1_3_recursion.py
+++ ch1_hello_world/ch1_3_recursion.py
@@ -3,6 +3,7 @@
 # https://www.justinmath.com/some-short-introductory-coding-exercises/
 import pdb
 
+# Starting with 5, generate each term by multiplying the previous term by 3 and subtracting 4.
 def problem1_array(terms):
     if not terms.isdecimal(): return 0
     n = int(terms)
@@ -32,13 +33,14 @@ def problem1_recursion_loop(n):
         prev_term = problem1_recursion_loop(n-1)
         return prev_term * 3 - 4
 
+# Starting with 25, generate each term by taking half of the previous term if it's even, 
+# or multiplying by 3 and adding 1 if it's odd. (This is an instance of a Collatz sequence.)
 def problem2_array(terms):
     if not terms.isdecimal(): return 0
     n = int(terms)
     if n < 1: return 0
 
     sequence = [25]
-    counter = 1
 
     if n == 1: return sequence
 
@@ -67,5 +69,62 @@ def problem2_recursion_loop(n):
     else: #even
         return int(prev_term / 2)
 
+# Starting with 0,1, generate each term by adding the previous two terms. (This is the famous Fibonacci sequence.)
+def problem3_array(terms):
+    if not str(terms).isdecimal(): return 0
+    n = int(terms)
+    if n < 2: return 0
 
+    sequence = [0, 1]
 
+    if n == 2: return sequence
+
+    for i in range(2, n):
+        sequence.append(sequence[i-2] + sequence[i-1])
+
+    return sequence
+
+def problem3_recursion(terms):
+    if not str(terms).isdecimal(): return 0
+    n = int(terms)
+    if n < 0: return 0
+
+    return problem3_recursion_loop(n, [0, 1])
+
+def problem3_recursion_loop(n, terms):
+    if n == 1:
+        return terms[0]
+    elif n == 2:
+        return terms[1] 
+
+    return problem3_recursion_loop(n-1, [terms[1], terms[0] + terms[1]])
+
+# Starting with 2,−3, generate each term by adding the product of the previous two terms.
+def problem4_array(terms):
+    if not str(terms).isdecimal(): return 0
+    n = int(terms)
+    if n < 2: return 0
+
+    sequence = [2, -3]
+
+    if n == 2: return sequence
+
+    for i in range(2, n):
+        sequence.append(sequence[i-2] * sequence[i-1])
+
+    return sequence
+
+def problem4_recursion(terms):
+    if not str(terms).isdecimal(): return 0
+    n = int(terms)
+    if n < 0: return 0
+
+    return problem4_recursion_loop(n, [2, -3])
+
+def problem4_recursion_loop(n, terms):
+    if n == 1:
+        return terms[0]
+    elif n == 2:
+        return terms[1] 
+
+    return problem4_recursion_loop(n-1, [terms[1], terms[0] * terms[1]])
--- ch1_hello_world/tests_ch1_3.py
+++ ch1_hello_world/tests_ch1_3.py
@@ -89,17 +89,157 @@ tests = [
     {
         'function' : problem2_recursion,
         'input' : ['1'],
-        'output' : 0
+        'output' : 25
     },
     {
         'function' : problem2_recursion,
         'input' : ['2'],
-        'output' : 0
+        'output' : 74
     },
     {
         'function' : problem2_recursion,
         'input' : ['3'],
+        'output' : 37
+    },
+    {
+        'function' : problem3_array,
+        'input' : ['-9'],
         'output' : 0
+    },
+    {
+        'function' : problem3_array,
+        'input' : ['©'],
+        'output' : 0
+    },
+    {
+        'function' : problem3_array,
+        'input' : ['1'],
+        'output' : 0
+    },
+    {
+        'function' : problem3_array,
+        'input' : ['2'],
+        'output' : [0, 1] 
+    },
+    {
+        'function' : problem3_array,
+        'input' : ['3'],
+        'output' : [0, 1, 1]
+    },
+    {
+        'function' : problem3_array,
+        'input' : ['4'],
+        'output' : [0, 1, 1, 2]
+    },
+    {
+        'function' : problem3_recursion,
+        'input' : ['-89'],
+        'output' : 0
+    },
+    {
+        'function' : problem3_recursion,
+        'input' : ['∫'],
+        'output' : 0
+    },
+    {
+        'function' : problem3_recursion,
+        'input' : ['1'],
+        'output' : 0
+    },
+    {
+        'function' : problem3_recursion,
+        'input' : ['2'],
+        'output' : 1
+    },
+    {
+        'function' : problem3_recursion,
+        'input' : ['3'],
+        'output' : 1
+    },
+    {
+        'function' : problem3_recursion,
+        'input' : ['4'],
+        'output' : 2
+    },
+    {
+        'function' : problem3_recursion,
+        'input' : ['5'],
+        'output' : 3
+    },
+    {
+        'function' : problem3_recursion,
+        'input' : ['8'],
+        'output' : 13
+    },
+    {
+        'function' : problem4_array,
+        'input' : ['-9'],
+        'output' : 0
+    },
+    {
+        'function' : problem4_array,
+        'input' : ['©'],
+        'output' : 0
+    },
+    {
+        'function' : problem4_array,
+        'input' : ['1'],
+        'output' : 0
+    },
+    {
+        'function' : problem4_array,
+        'input' : ['2'],
+        'output' : [2, -3] 
+    },
+    {
+        'function' : problem4_array,
+        'input' : ['3'],
+        'output' : [2, -3, -6]
+    },
+    {
+        'function' : problem4_array,
+        'input' : ['4'],
+        'output' : [2, -3, -6, 18]
+    },
+    {
+        'function' : problem4_recursion,
+        'input' : ['-89'],
+        'output' : 0
+    },
+    {
+        'function' : problem4_recursion,
+        'input' : ['∫'],
+        'output' : 0
+    },
+    {
+        'function' : problem4_recursion,
+        'input' : ['1'],
+        'output' : 2
+    },
+    {
+        'function' : problem4_recursion,
+        'input' : ['2'],
+        'output' : -3
+    },
+    {
+        'function' : problem4_recursion,
+        'input' : ['3'],
+        'output' : -6
+    },
+    {
+        'function' : problem4_recursion,
+        'input' : ['4'],
+        'output' : 18
+    },
+    {
+        'function' : problem4_recursion,
+        'input' : ['5'],
+        'output' : -108
+    },
+    {
+        'function' : problem4_recursion,
+        'input' : ['8'],
+        'output' : -408146688
     }
 ]
 
