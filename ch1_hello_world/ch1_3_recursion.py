063935a8816bac6fa68c57a995839e39adc784d6
sbrokaw <brok.sea@gmail.com>
2025-09-09 13:54:03 -0700

    recursion work

--- ch1_hello_world/ch1_3_recursion.py
+++ ch1_hello_world/ch1_3_recursion.py
@@ -2,3 +2,70 @@
 # by Justin Skycak
 # https://www.justinmath.com/some-short-introductory-coding-exercises/
 import pdb
+
+def problem1_array(terms):
+    if not terms.isdecimal(): return 0
+    n = int(terms)
+    if n < 1: return 0
+
+    sequence = [5]
+    counter = 1
+
+    if n == 1: return sequence
+
+    for i in range(1, n):
+        sequence.append(sequence[i-1] * 3 - 4)
+
+    return sequence
+
+def problem1_recursion(terms):
+    if not terms.isdecimal(): return 0
+    n = int(terms)
+    if n < 1: return 0
+
+    return problem1_recursion_loop(n)
+
+def problem1_recursion_loop(n):
+    if n == 1: 
+        return 5
+    else:
+        prev_term = problem1_recursion_loop(n-1)
+        return prev_term * 3 - 4
+
+def problem2_array(terms):
+    if not terms.isdecimal(): return 0
+    n = int(terms)
+    if n < 1: return 0
+
+    sequence = [25]
+    counter = 1
+
+    if n == 1: return sequence
+
+    for i in range(1, n):
+        if sequence[i-1] % 2:
+            sequence.append(int(sequence[i-1] * 3 - 1))
+        else:
+            sequence.append(int(sequence[i-1] / 2))
+
+    return sequence
+
+def problem2_recursion(terms):
+    if not terms.isdecimal(): return 0
+    n = int(terms)
+    if n < 1: return 0
+
+    return problem2_recursion_loop(n)
+
+def problem2_recursion_loop(n):
+    if n == 1:
+        return 25
+
+    prev_term = problem2_recursion_loop(n-1)
+    if prev_term % 2: #odd
+        return prev_term * 3 - 1
+    else: #even
+        return int(prev_term / 2)
+
+
+
--- ch1_hello_world/tests_ch1_3.py
+++ ch1_hello_world/tests_ch1_3.py
@@ -1,14 +1,111 @@
-from ch1_3_recursion.py import *
+from ch1_3_recursion import *
 
 tests = [
     {
-        'function' = 'problem1_array',
-        'input' = ['3'],
-        'output' = ''
+        'function' : problem1_array,
+        'input' : ['b'],
+        'output' : 0
+    },
+    {
+        'function' : problem1_array,
+        'input' : ['-10'],
+        'output' : 0
+    },
+    {
+        'function' : problem1_array,
+        'input' : ['1'],
+        'output' : [5]
+    },
+    {
+        'function' : problem1_array,
+        'input' : ['2'],
+        'output' : [5, 11]
+    },
+    {
+        'function' : problem1_array,
+        'input' : ['3'],
+        'output' : [5, 11, 29]
+    },
+    {
+        'function' : problem1_recursion,
+        'input' : ['b'],
+        'output' : 0
+    },
+    {
+        'function' : problem1_recursion,
+        'input' : ['-10'],
+        'output' : 0
+    },
+    {
+        'function' : problem1_recursion,
+        'input' : ['1'],
+        'output' : 5
+    },
+    {
+        'function' : problem1_recursion,
+        'input' : ['2'],
+        'output' : 11
+    },
+    {
+        'function' : problem1_recursion,
+        'input' : ['3'],
+        'output' : 29
+    },
+    {
+        'function' : problem2_array,
+        'input' : ['-99'],
+        'output' : 0
+    },
+    {
+        'function' : problem2_array,
+        'input' : ['$'],
+        'output' : 0
+    },
+    {
+        'function' : problem2_array,
+        'input' : ['1'],
+        'output' : [25]
+    },
+    {
+        'function' : problem2_array,
+        'input' : ['2'],
+        'output' : [25, 74]
+    },
+    {
+        'function' : problem2_array,
+        'input' : ['3'],
+        'output' : [25, 74, 37]
+    },
+    {
+        'function' : problem2_recursion,
+        'input' : ['-99'],
+        'output' : 0
+    },
+    {
+        'function' : problem2_recursion,
+        'input' : ['∆'],
+        'output' : 0
+    },
+    {
+        'function' : problem2_recursion,
+        'input' : ['1'],
+        'output' : 0
+    },
+    {
+        'function' : problem2_recursion,
+        'input' : ['2'],
+        'output' : 0
+    },
+    {
+        'function' : problem2_recursion,
+        'input' : ['3'],
+        'output' : 0
     }
 ]
 
 
+successes = 0
+failures = 0
 
 print("=".center(50, '='))
 print(" Chapter 1.3 Tests ".center(50, '='))
@@ -23,3 +120,13 @@ for test in tests:
         result = function(*trial)
     else:
         result = function(trial)
+
+    if result == goal:
+        successes += 1
+    else:
+        failures += 1
+        print(f'!ch1_3_test FAIL! {function.__name__}({trial})')
+        print(f'!ch1_3_test FAIL!   returned {result}')
+        print(f'!ch1_3_test FAIL!   expected {goal}')
+
+print(f'Testing complete with {successes} passes and {failures} failures')
