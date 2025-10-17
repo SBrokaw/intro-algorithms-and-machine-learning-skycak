f3395976b8d8855cb9ce1c8f10a556282f0fba36
sbrokaw <brok.sea@gmail.com>
2025-10-17 15:53:04 -0700

    formatting the enigma decoded possibilities better

--- ch2_searching_and_sorting/ch2_1_brute_force_search.py
+++ ch2_searching_and_sorting/ch2_1_brute_force_search.py
@@ -3,3 +3,48 @@
 # Strategic Agents. 
 # https://justinmath.com/brute-force-search-with-linear-encoding-cryptography/
 
+def encode_msg( msg, a, b ):
+    #max() function handles case where m is a ' ' space. ord(' ') == 32
+    ordinal_msg = [max(0, ord(m) - 96) for m in msg.lower()] 
+    encoded_msg = [o * a + b for o in ordinal_msg]
+
+    print(f'[DEBUG 10] Input:   {msg}')
+    print(f'[DEBUG 10] Ordinal: {ordinal_msg}')
+    print(f'[DEBUG 10] Encoded: {encoded_msg}')
+
+    return encoded_msg
+
+def decode_msg( msg, a, b ):
+    if a == 0 or b == 0: return False
+    decoded_msg = [int((m - b) / a) for m in msg]
+
+    legible_check = set(decoded_msg)
+    if any( x < 0 or x > 26 or x % 1 != 0 for x in legible_check):
+        return False
+    else:
+        chr_msg = [chr(96 + m) for m in decoded_msg]
+        return chr_msg
+
+    return False
+
+
+def enigma_decode( secret ):
+    possibilities = [[]]
+    possibilities = [p + [decoded] 
+                     for p in possibilities
+                     for i in range(101)
+                     for j in range(101)
+                     if( decoded := decode_msg( secret, i, j ) )]
+    legible = []
+    for p in possibilities:
+        for c in p:
+            legible.append(''.join(c))
+
+    print(f'[Enigma Possibilities]')
+
+encode_msg("a cat", 2, 3)
+decode_msg(encode_msg("a cat", 2, 3), 2, 3)
+decode_msg(encode_msg("a cat", 2, 3), 4, 5)
+
+secret = [377, 717, 71, 513, 105, 921, 581, 547, 547, 105, 377, 717, 241, 71, 105, 547, 71, 377, 547, 717, 751, 683, 785, 513, 241, 547, 751]
+enigma_decode(secret)
