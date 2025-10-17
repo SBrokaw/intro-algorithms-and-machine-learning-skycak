5a7a214063b68beceb41acf9f35cae31fc006665
sbrokaw <brok.sea@gmail.com>
2025-10-17 13:58:53 -0700

    chapter 2

---
+++ ch2_searching_and_sorting/ch2_1_brute_force_search.py
@@ -0,0 +1,5 @@
+# Skycak, J. (2021). Brute Force Search with Linear-Encoding Cryptography. 
+# In Introduction to Algorithms and Machine Learning: from Sorting to 
+# Strategic Agents. 
+# https://justinmath.com/brute-force-search-with-linear-encoding-cryptography/
+
