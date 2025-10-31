4d64b294af9bbbca28a04d77846f1573bfb266d9
sbrokaw <brok.sea@gmail.com>
2025-10-31 09:45:52 -0700

    finished bisection search. Newton-Raphson next.

--- ch2_searching_and_sorting/ch2_3_estimating_roots.py
+++ ch2_searching_and_sorting/ch2_3_estimating_roots.py
@@ -5,6 +5,7 @@
 
 # calculates the x^(1/y) within precision
 def calc_root_bisection(x, y, precision):
+    if x <= 0 or y <= 0: return 0
     a = 0.0                # lower bound
     b = float(x)           # upper bound
     c = float((b - a) / 2) # midpoint
@@ -12,15 +13,19 @@ def calc_root_bisection(x, y, precision):
 
     print('x    y    a     b     c     b-c    trial')
     print('¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯')
-    while( b - c > err ):
+    trial = c ** y - x
+    while( abs(trial) > err ):
         trial = c ** y - x
         print(f'{x:<4} {y:<4} {a:<5.3g} {b:<5.3g} {c:<5.3g} {b-c:<5.3g} {trial:<4.3g}')
         if trial > (0 + err): b = c
         elif trial < (0 - err): a = c
 
-        c = (b - a) / 2 # update midpoint
+        c = (b + a) / 2 # update midpoint
 
-    print(f'[{a:.5g}, {c:.5g}, {b:.5g}], {y}√{x} = {trial:.5g}')
-    return trial
+    print(f'[{a:.7g}, {c:.7g}, {b:.7g}], {y}√{x} = {c:.7g}')
+    return c
 
 calc_root_bisection( 2, 3, 0.05 )
+calc_root_bisection( 2, 3, 0.005 )
+calc_root_bisection( 2, 3, 0.0005 )
+calc_root_bisection( 2, 3, 0.0000001 )
