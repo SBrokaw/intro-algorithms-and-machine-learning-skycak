d6983622cb92ed37a02862303f66a5d68a9d63c7
sbrokaw <brok.sea@gmail.com>
2025-11-02 23:12:57 -0800

    starting

--- ch2_searching_and_sorting/ch2_4_single_variable_gradient_descent.py
+++ ch2_searching_and_sorting/ch2_4_single_variable_gradient_descent.py
@@ -1,1 +1,4 @@
-Skycak, J. (2021). Single-Variable Gradient Descent. In Introduction to Algorithms and Machine Learning: from Sorting to Strategic Agents. https://justinmath.com/single-variable-gradient-descent/
+# Skycak, J. (2021). Single-Variable Gradient Descent. 
+# In Introduction to Algorithms and Machine Learning: 
+# from Sorting to Strategic Agents. 
+# https://justinmath.com/single-variable-gradient-descent/
