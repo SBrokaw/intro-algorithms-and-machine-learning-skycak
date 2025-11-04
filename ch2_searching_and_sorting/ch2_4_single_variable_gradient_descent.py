802b9a6a79f036da4ed09984cf0342f58a58e18f
sbrokaw <brok.sea@gmail.com>
2025-11-03 16:50:22 -0800

    starting gradient descent

--- ch2_searching_and_sorting/ch2_4_single_variable_gradient_descent.py
+++ ch2_searching_and_sorting/ch2_4_single_variable_gradient_descent.py
@@ -2,3 +2,27 @@
 # In Introduction to Algorithms and Machine Learning: 
 # from Sorting to Strategic Agents. 
 # https://justinmath.com/single-variable-gradient-descent/
+import math
+
+def next_guess( p, m, alpha ):
+    return p - alpha * m 
+
+def slope_tangent_exercise0( p ):
+    return 2 * p
+
+def slope_tangent_exercise1( p ):
+    return 2 * p + 1
+
+def slope_tangent_exercise2( p ):
+    return 3 * p ** 2 - 4 * p ** 3 - 2 * p
+
+def slope_tangent_exercise3( p ):
+    return 0
+
+def exercise0( alpha, trials ):
+    initial_guess = 1.0
+    print('n   x_n       f\'(x_n)  α∙f\'(x_n)')
+    print('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
+    for n in range(trials):
+
+
