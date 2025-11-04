7c39dba3202fc4ca1c378d2ecaeccc45d796f731
sbrokaw <brok.sea@gmail.com>
2025-11-04 14:59:43 -0800

    finished intro exercise0, move onto exercise1 next with random start points

--- ch2_searching_and_sorting/ch2_4_single_variable_gradient_descent.py
+++ ch2_searching_and_sorting/ch2_4_single_variable_gradient_descent.py
@@ -7,6 +7,9 @@ import math
 def next_guess( p, m, alpha ):
     return p - alpha * m 
 
+def point_exercise0( p ):
+    return p ** 2
+
 def slope_tangent_exercise0( p ):
     return 2 * p
 
@@ -20,13 +23,17 @@ def slope_tangent_exercise3( p ):
     return 0
 
 def exercise0( alpha, trials ):
-    output_mask = {1, 2, 3, 25, 50, 100, 200, 300, 400}
-    initial_guess = 1.0
-    print('n   x_n       f\'(x_n)  α∙f\'(x_n)')
-    print('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
+    output_mask = {0, 1, 2, 3, 25, 50, 100, 200, 300, 400}
+    x_n = 1.0
+    print('n   x_n      f\'(x_n)  α∙f\'(x_n) y_n')
+    print('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
     for n in range(trials):
-        if n+1 in output_mask: print(f'{n+1:<4g}')
+        m = slope_tangent_exercise0( x_n )
+        y_n = point_exercise0( x_n )
+        if n in output_mask: print(f'{n:<3g} {x_n:<6.6f} {m:<7.6f} {alpha * m:<9.6f} {y_n:<8.6f}')
 
-    return 0
+        x_n -= alpha * m
 
-exercise0( 0.01, 400 )
+    return y_n
+
+exercise0( 0.01, 401 )
