049bbb2b1526a780b17572bf67f54054a6c340af
sbrokaw <brok.sea@gmail.com>
2025-11-05 15:53:00 -0800

    starting Multi-Variable Gradient Descent

---
+++ ch2_searching_and_sorting/ch2_5_multi_variable_gradient_descent.py
@@ -0,0 +1,3 @@
+# Skycak, J. (2021). Multivariable Gradient Descent. In Introduction to 
+# Algorithms and Machine Learning: from Sorting to Strategic Agents. 
+# https://justinmath.com/multivariable-gradient-descent/
