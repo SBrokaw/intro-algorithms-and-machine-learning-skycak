fefb3212b341ebcc78b60ecee1df70ded79e8571
sbrokaw <brok.sea@gmail.com>
2025-11-21 15:58:33 -0800

    Starting matrix_multiply() function. Dot product method.

--- ch3_objects/ch3_1_matrix_arithmetic.py
+++ ch3_objects/ch3_1_matrix_arithmetic.py
@@ -2,10 +2,22 @@
 # Algorithms and Machine Learning: from Sorting to Strategic Agents. 
 # https://justinmath.com/basic-matrix-arithmetic/
 
+def dot_product( arr1, arr2 ):
+    if len(arr1) != len(arr2):
+        print(f'ERROR! Dimension mismatch. {len(arr1)} != {len(arr2)}')
+        return 0
+
+    product = 0
+    for i in range(len(arr1)):
+        product += arr1[i] * arr2[i]
+
+    return product
+
 class Matrix:
     def __init__(self, rows):
        self.data = [r for r in rows] 
-       print(f'init {self.data}')
+       self.num_cols = len(self.data[0])
+       self.num_rows = len(self.data)
 
     def show(self):
         for r in self.data:
@@ -35,9 +47,54 @@ class Matrix:
 
         return Matrix(transposed)
 
+    def add(self, m):
+        if self.num_rows != m.num_rows or self.num_cols != m.num_cols:
+            print(f'ERROR! Matrix dimension mismatch. [{self.num_rows}, {self.num_cols}] != [{m.num_rows}, {m.num_cols}]')
+            return self
 
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                self.data[i][j] += m.data[i][j]
 
-A = Matrix([[1, 2], [3, 4]])
-A.show()
-A_t = A.transpose()
-A_t.disp()
+        return self
+
+    def subtract(self, m):
+        if self.num_rows != m.num_rows or self.num_cols != m.num_cols:
+            print(f'ERROR! Matrix dimension mismatch. [{self.num_rows}, {self.num_cols}] != [{m.num_rows}, {m.num_cols}]')
+            return self
+
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                self.data[i][j] -= m.data[i][j]
+
+        return self
+
+    def scalar_multiply(self, k):
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                self.data[i][j] *= k
+
+        return self
+
+    def matrix_multiply(self, m):
+        if self.num_cols != m.num_rows:
+            print(f'ERROR! Matrix dimension mismatch. [{self.num_rows}, {self.num_cols}] != [{m.num_rows}, {m.num_cols}]')
+            return self
+
+        product = [[0] * self.num_rows for c in range(self.num_cols)]
+        for i in range(len(product.data)):
+            for j in range(len(product.data[i])):
+                product[i][j] = dot_product(row, col) #TODO
+
+matxs = [Matrix([[1, 2], [3, 4]]),
+         Matrix([[1, 2, 3], [4, 5, 6]]),
+         Matrix([[-1, -2], [-3, -4], [-5, -6]])]
+
+for A in matxs:
+    A.show()
+    A_t = A.transpose()
+    A_t.disp()
+    A.add(A_t).print()
+    A.subtract(A_t).print()
+    A.scalar_multiply(3).print()
+    print()
