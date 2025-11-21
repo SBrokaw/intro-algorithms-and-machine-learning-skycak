24a7b01510e2b765d163bc203712747a7a584785
sbrokaw <brok.sea@gmail.com>
2025-11-21 10:22:21 -0800

    finished tranpose() and show(). Tricky list aliasing issues that I still run into every time I am messing with arrays in Python. Oh well.

--- ch3_objects/ch3_1_matrix_arithmetic.py
+++ ch3_objects/ch3_1_matrix_arithmetic.py
@@ -14,7 +14,30 @@ class Matrix:
                 end_char = ']' if i == len(r) - 1 else ', '
                 print(f'{r[i]} {end_char}', end = '')
             print('')
+        return 0
+
+    def print(self):
+        return self.show()
+
+    def disp(self):
+        return self.show()
+    
+    def display(self):
+        return self.show()
+
+    def transpose(self):
+        rows = len(self.data)
+        columns = len(self.data[0])
+        transposed = [[0] * rows for c in range(columns)]
+        for i in range(rows):
+            for j in range(columns):
+                transposed[j][i] = self.data[i][j]
+
+        return Matrix(transposed)
+
 
 
 A = Matrix([[1, 2], [3, 4]])
 A.show()
+A_t = A.transpose()
+A_t.disp()
