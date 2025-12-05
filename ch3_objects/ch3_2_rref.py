4550ca3a3d48870fb3ee816d22cfb48acde4b023
sbrokaw <brok.sea@gmail.com>
2025-12-04 21:18:24 -0800

    Finished RREF exercises. PR and then onto k-means clustering.

--- ch3_objects/ch3_2_rref.py
+++ ch3_objects/ch3_2_rref.py
@@ -3,6 +3,12 @@
 # Machine Learning: from Sorting to Strategic Agents. 
 # https://justinmath.com/reduced-row-echelon-form-and-applications-to-matrix-arithmetic/
 
+def matrix_print(rows, indent_n):
+    indent = ' ' * indent_n
+    for r in rows:
+        r_format = [f'{i: .3g}' for i in r]
+        print(f'{indent}{r_format}')
+
 class Matrix:
     def __init__(self, rows):
         self.data = [r for r in rows]
@@ -21,7 +27,7 @@ class Matrix:
             print('[ ', end = '')
             for i in range(len(r)):
                 end_char = '' if i == len(r) - 1 else ', '
-                print(f'{r[i]:.3g} {end_char}', end = '')
+                print(f'{r[i]: .3g} {end_char}', end = '')
             print(']')
         return 0
 
@@ -68,7 +74,7 @@ class Matrix:
             # if pivot row exists for column:
             col_i = [rref[u][i] for u in range(self.num_rows)]
             pivot_idx = next((j for j, v in enumerate(col_i) if v != 0 and j >= row_idx), -1)
-            print(f'  swap? pivot_idx:{pivot_idx} row_idx:{row_idx} {rref}')
+            # print(f'  swap? pivot_idx:{pivot_idx} row_idx:{row_idx} {rref}')
             if pivot_idx >= 0:
                 # if pivot row does not match current row_index:
                 if pivot_idx != row_idx:
@@ -77,35 +83,139 @@ class Matrix:
                     temp_row = rref[row_idx]
                     rref[row_idx] = rref[pivot_idx]
                     rref[pivot_idx] = temp_row
-                    print(f'  swap {row_idx},{pivot_idx}\t{rref}')
+                    # print(f'  swap {row_idx},{pivot_idx}\t{rref}')
 
                 # divide pivot row (so that first nonzero entry is 1)
                 scalar = rref[row_idx][i]
                 if scalar != 0 and scalar != 1:
                     rref[row_idx] = [k / scalar for k in rref[row_idx]]
-                    print(f'  scale r{row_idx} 1/{scalar}\t{rref}')
+                    # print(f'  scale r{row_idx} 1/{scalar}\t{rref}')
 
                 # clear entries below and above pivot entry
                 # (by subtracting multiples of pivot row)
                 clr_rows = [u for u in range(len(self.col(i))) if u != row_idx and rref[u][i] != 0]
-                print(f'  clr_rows:{clr_rows} i:{i} row_idx:{row_idx} {rref}')
+                # print(f'  clr_rows:{clr_rows} i:{i} row_idx:{row_idx} {rref}')
                 for clr in clr_rows:
                     if rref[row_idx][i] == 0: continue
                     k = rref[clr][i] / rref[row_idx][i]
                     rref[clr] = [rref[clr][u] - k * rref[row_idx][u] for u in range(len(rref[clr]))]
-                    print(f'  clear r{clr} - {k}∙r{row_idx}\t{rref}')
+                    # print(f'  clear r{clr} - {k}∙r{row_idx}\t{rref}')
 
                 row_idx += 1
 
         return Matrix(rref)
 
-    # TODO
     def inverse(self):
-        return Matrix([[]])
+        if self.data == [[]]: 
+            print(f'ERROR! Inverse does not exist for matrix {self.data}')
+            return Matrix([[]])
+        if self.num_cols != self.num_rows: 
+            print(f'ERROR! Inverse does not exist for non-square {self.num_rows}x{self.num_cols} matrix ')
+            return Matrix([[]])
 
-    # TODO
+        rref = [r[:] for r in self.data]
+        for idx, r in enumerate(rref):
+            augment = [0] * self.num_cols
+            augment[idx] = 1
+            rref[idx] += augment
+
+        row_idx = 0
+        pivot_idx = 0
+        # matrix_print(rref, 2)
+        for i in range(self.num_cols):
+            # if pivot row exists for column:
+            col_i = [rref[u][i] for u in range(self.num_rows)]
+            pivot_idx = next((j for j, v in enumerate(col_i) if v != 0 and j >= row_idx), -1)
+            # print(f'  swap? pivot_idx:{pivot_idx} row_idx:{row_idx}'); matrix_print(rref, 4)
+            if pivot_idx >= 0:
+                # if pivot row does not match current row_index:
+                if pivot_idx != row_idx:
+                    # swap current row with pivot row
+                    # (so that it matches)
+                    temp_row = rref[row_idx]
+                    rref[row_idx] = rref[pivot_idx]
+                    rref[pivot_idx] = temp_row
+                    # print(f'  swapped {row_idx},{pivot_idx}'); matrix_print(rref, 4)
+
+                # divide pivot row (so that first nonzero entry is 1)
+                scalar = rref[row_idx][i]
+                if scalar != 0 and scalar != 1:
+                    rref[row_idx] = [k / scalar for k in rref[row_idx]]
+                    # print(f'  scaled r{row_idx} 1/{scalar}'); matrix_print(rref, 4)
+
+                # clear entries below and above pivot entry
+                # (by subtracting multiples of pivot row)
+                clr_rows = [u for u in range(len(self.col(i))) if u != row_idx and rref[u][i] != 0]
+                # print(f'  clr_rows:{clr_rows} i:{i} row_idx:{row_idx}'); matrix_print(rref, 4)
+                for clr in clr_rows:
+                    if rref[row_idx][i] == 0: continue
+                    k = rref[clr][i] / rref[row_idx][i]
+                    rref[clr] = [rref[clr][u] - k * rref[row_idx][u] for u in range(len(rref[clr]))]
+                    # print(f'  cleared r{clr} - {k}∙r{row_idx}'); matrix_print(rref, 4)
+
+                row_idx += 1
+
+            else: # no pivot -> singular matrix -> no inverse exists
+                print(f'ERROR! Inverse does not exist for singular matrix.')
+                return Matrix([[]])
+
+        inverse = [r[self.num_cols:] for r in rref]
+        return Matrix(inverse)
+
     def determinant(self):
-        return 0
+        if self.data == [[]]: 
+            print(f'ERROR! Determinant does not exist for matrix {self.data}')
+            return None
+        if self.num_cols != self.num_rows: 
+            print(f'ERROR! Determinant does not exist for non-square {self.num_rows}x{self.num_cols} matrix ')
+            return None
+
+        rref = [r[:] for r in self.data]
+
+        row_idx = 0
+        pivot_idx = 0
+        det_product = 1
+        det_sign = 1
+        # matrix_print(rref, 2)
+        for i in range(self.num_cols):
+            # if pivot row exists for column:
+            col_i = [rref[u][i] for u in range(self.num_rows)]
+            pivot_idx = next((j for j, v in enumerate(col_i) if v != 0 and j >= row_idx), -1)
+            # print(f'  swap? pivot_idx:{pivot_idx} row_idx:{row_idx}'); matrix_print(rref, 4)
+            if pivot_idx >= 0:
+                # if pivot row does not match current row_index:
+                if pivot_idx != row_idx:
+                    # swap current row with pivot row
+                    # (so that it matches)
+                    temp_row = rref[row_idx]
+                    rref[row_idx] = rref[pivot_idx]
+                    rref[pivot_idx] = temp_row
+                    det_sign *= -1
+                    # print(f'  swapped {row_idx},{pivot_idx}'); matrix_print(rref, 4)
+
+                # divide pivot row (so that first nonzero entry is 1)
+                scalar = rref[row_idx][i]
+                if scalar != 0 and scalar != 1:
+                    rref[row_idx] = [k / scalar for k in rref[row_idx]]
+                    det_product *= scalar
+                    # print(f'  scaled r{row_idx} 1/{scalar}'); matrix_print(rref, 4)
+
+                # clear entries below and above pivot entry
+                # (by subtracting multiples of pivot row)
+                clr_rows = [u for u in range(len(self.col(i))) if u != row_idx and rref[u][i] != 0]
+                # print(f'  clr_rows:{clr_rows} i:{i} row_idx:{row_idx}'); matrix_print(rref, 4)
+                for clr in clr_rows:
+                    if rref[row_idx][i] == 0: continue
+                    k = rref[clr][i] / rref[row_idx][i]
+                    rref[clr] = [rref[clr][u] - k * rref[row_idx][u] for u in range(len(rref[clr]))]
+                    # print(f'  cleared r{clr} - {k}∙r{row_idx}'); matrix_print(rref, 4)
+
+                row_idx += 1
+
+            else: # no pivot -> singular matrix -> det(A) == 0
+                return 0
+
+        return det_sign * det_product
 
     def rref_old(self):
         if self.data == [[]]: 
@@ -280,7 +390,9 @@ matxs = [
          Matrix([[0, 5, -1], [0, 0, -1], [-1, 0, -1]]),
          Matrix([[0, 0, 0], [0, 2, 1], [1, -1, 0]]),
          Matrix([[1, 0, -1], [0, 2, 1], [1, -1, 0]]),
+         Matrix([[9, 0, 0], [0, 0, 0], [0, 0, 7]]),
          Matrix([[0, 0, 0], [0, 2, 0], [0, -1, 0]]),
+         Matrix([[3, 0, 0], [0, 0, 0], [0, 0, 0]]),
          Matrix([[-1, 1, -1], [0, -1, -1]]),
          Matrix([[-1, 1], [0, -1], [1, 0]])
          ]
