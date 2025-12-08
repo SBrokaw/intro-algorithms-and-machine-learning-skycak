175e214bfc5fc94c7b5cfac96f1dfa8db58a70ed
sbrokaw <brok.sea@gmail.com>
2025-12-08 14:58:43 -0800

    OOP practice using simple games.

---
+++ ch3_objects/ch3_4_tictactoe.py
@@ -0,0 +1,3 @@
+# Skycak, J. (2021). Tic-Tac-Toe and Connect Four. In Introduction to
+# Algorithms and Machine Learning: from Sorting to Strategic Agents. 
+# https://justinmath.com/tic-tac-toe-and-connect-four/
