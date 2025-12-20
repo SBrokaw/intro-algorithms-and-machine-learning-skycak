1d3cb8f1633f493c132eb66cd9814031947a7b56
sbrokaw <brok.sea@gmail.com>
2025-12-19 22:26:43 -0800

    looks like seaborn is recommended for plotting. https://seaborn.pydata.org/tutorial/introduction

--- ch3_objects/watch.sh
+++ ch3_objects/watch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-while fswatch -1 ch3_4_connectfour.py; do
+while fswatch -1 ch3_5_euler_estimation.py; do
     clear
-    python ch3_4_connectfour.py
+    python ch3_5_euler_estimation.py
 done
