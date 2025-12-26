6a6fa54e97da86b3bfb97c62661688a7fe2cc848
sbrokaw <brok.sea@gmail.com>
2025-12-26 06:30:27 -0800

    Finished multivariable Euler estimator. \*SIR Model For the Spread of Disease\* next.

--- ch3_objects/ch3_5_euler_estimation.py
+++ ch3_objects/ch3_5_euler_estimation.py
@@ -2,7 +2,6 @@
 # and Machine Learning: from Sorting to Strategic Agents. 
 # https://justinmath.com/euler-estimation/
 from math import e
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 class EulerEstimator():
@@ -22,6 +21,28 @@ class EulerEstimator():
 
         return points
 
+class EulerEstimatorMultivariable():
+    def __init__(self, derivatives):
+        self.derivatives = derivatives
+
+    def eval_derivative(self, key, t, p):
+        return self.derivatives[key](t, p)
+
+    def estimate_points(self, p0, dt, n):
+        t = p0[0]
+        p = p0[1]
+        a, b, c = p['a'], p['b'], p['c']
+        points = [(t, p)]
+        for i in range(n):
+            a += dt * self.eval_derivative('a', t, p)
+            b += dt * self.eval_derivative('b', t, p)
+            c += dt * self.eval_derivative('c', t, p)
+            t += dt 
+            p = {'a':a, 'b':b, 'c':c}
+            points += [(t, p)]
+            # print(f"  {t}, {p}")
+
+        return points
 
 def derivative_00(p):
     return p[0] + 1
@@ -32,6 +53,17 @@ def derivative_01(p):
 
 def derivative_02(p):
     return p[0] - 2
+
+def da_dt_03(t, p):
+    val = p["a"] + 1
+    # print(f"    da_dt_03 t:{t} p:{p} = {val}")
+    return val
+
+def db_dt_03(t, p):
+    return p["a"] + p["b"]
+
+def dc_dt_03(t, p):
+    return 2 * p["b"] + 3 * t
 
 
 p0 = (1, 4)
@@ -52,8 +84,8 @@ for i in range(len(points)): print(f"p{i:.3g} ({points[i][0]:.3g}, {points[i][1]
 
 
 p0_options = [(0, -2), (0, -1), (0, 0), (0, 1), (0, 2)]
-dx = 0.1
 n = 50
+dx = 5 / n
 points = []
 euler = EulerEstimator(derivative_02)
 for p0 in p0_options:
@@ -73,5 +105,17 @@ plt.title("Euler Estimation: dy/dx = x - 2")
 plt.legend()
 plt.grid(True)
 
-plt.savefig("euler_estimation_02.png")
+# plt.savefig("euler_estimation_02.png")
+print("  figure plotted")
+print()
 
+
+p0 = (-0.4, {'a': -0.45, 'b': -0.05, 'c': 0})
+n = 3
+dt = 2
+derivatives = {'a': da_dt_03, 'b': db_dt_03, 'c': dc_dt_03}
+euler_mv = EulerEstimatorMultivariable(derivatives)
+points = euler_mv.estimate_points(p0, dt, n)
+print(f"{[euler_mv.derivatives[key].__name__ for key in derivatives]}".center(20, '—'))
+# for i in range(len(points)): print(f"p{i:.3g} ({points[0]}, {points[1]})")
+for p in points: print(p)
