c1a4f1b8e37f15bdcab44489306efed711c8240a
sbrokaw <brok.sea@gmail.com>
2025-12-25 21:33:31 -0800

    Charted Euler estimates using MatPlotLib with minimal other stuffis. Generates a .png right now.

--- ch3_objects/ch3_5_euler_estimation.py
+++ ch3_objects/ch3_5_euler_estimation.py
@@ -2,6 +2,8 @@
 # and Machine Learning: from Sorting to Strategic Agents. 
 # https://justinmath.com/euler-estimation/
 from math import e
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 class EulerEstimator():
     def __init__(self, derivative):
@@ -47,3 +49,29 @@ euler = EulerEstimator(derivative_01)
 points = euler.estimate_points(p0, dx, n)
 print(f"{euler.derivative.__name__}".center(20, '—'))
 for i in range(len(points)): print(f"p{i:.3g} ({points[i][0]:.3g}, {points[i][1]:.3g})")
+
+
+p0_options = [(0, -2), (0, -1), (0, 0), (0, 1), (0, 2)]
+dx = 0.1
+n = 50
+points = []
+euler = EulerEstimator(derivative_02)
+for p0 in p0_options:
+    points += [euler.estimate_points(p0, dx, n)]
+print(f"{euler.derivative.__name__}".center(20, '—'))
+# for i in range(len(points)): print(f"p{i:.3g} ({points[i][0]:.3g}, {points[i][1]:.3g})")
+
+plt.figure()
+for curve in points:
+    xss = [v[0] for v in curve]
+    yss = [v[1] for v in curve]
+    plt.plot(xss, yss, label=f"f({xss[0]}) = {yss[0]}")
+
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Euler Estimation: dy/dx = x - 2")
+plt.legend()
+plt.grid(True)
+
+plt.savefig("euler_estimation_02.png")
+
