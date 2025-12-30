fe5aaeb86b9f31b40d02546da61746edf9905347
sbrokaw <brok.sea@gmail.com>
2025-12-30 06:27:00 -0800

    something's up with my equations... the peaks are attenuated vs. the assignment.

--- ch3_objects/ch3_7_hodgkin_huxley.py
+++ ch3_objects/ch3_7_hodgkin_huxley.py
@@ -29,15 +29,17 @@ class EulerEstimatorMultivariable():
         p = p0[1]
         a, b, c, d = p['V'], p['n'], p['m'], p['h']
         points = [(t, p)]
+        # print(f"  {t:.3g}, {p}")
         for i in range(n):
-            a += 0 #dt * self.eval_derivative('S', t, p)
-            b += 0 #dt * (self.eval_derivative('I', t, p) - self.eval_derivative('R', t, p))
-            c += 0 #dt * self.eval_derivative('R', t, p)  
-            d += 0 #dt * self.eval_derivative('R', t, p)  
+            a += dt * self.eval_derivative('V', t, p)
+            b += dt * self.eval_derivative('n', t, p)
+            c += dt * self.eval_derivative('m', t, p)  
+            d += dt * self.eval_derivative('h', t, p)  
             t += dt 
             p = {'V':a, 'n':b, 'm':c, 'h':d}
             points += [(t, p)]
-            print(f"  {t}, {p}")
+            debug_str = [f"{t:.3g}"] + [f"{pp}:{p[pp]:.3g} " for pp in p]
+            # print(f"  {debug_str}")
 
         return points
 
@@ -54,6 +56,24 @@ def stimulus(t):
     stim = 150 if any(lo <= t and t <= hi for lo, hi in intervals) else 0
 
     return stim
+
+def g_Na(m, h):
+    return _g_Na * (m ** 3) * h
+
+def g_K(n):
+    return _g_K * (n ** 4)
+
+def g_L():
+    return _g_L
+
+def I_Na(t, p):
+    return g_Na(p['m'], p['h']) * (p['V'] - _V_Na)
+
+def I_K(t, p):
+    return g_K(p['n']) * (p['V'] - _V_K)
+
+def I_L(t, p):
+    return _g_L * (p['V'] - _V_L)
 
 def alpha_n(V):
     return 0.01 * (10 - V) / (exp(0.1 * (10 - V)) - 1)
@@ -74,7 +94,7 @@ def Beta_h(V):
     return 1 / (exp(0.1 * (30 - V)) + 1)
 
 def dV_dt(t, p):
-    return (1 / _C) * (stimulus(t))
+    return (1 / _C) * (stimulus(t) - I_Na(t, p) - I_K(t, p) - I_L(t, p))
 
 def dn_dt(t, p):
     return alpha_n(p['V']) * (1 - p['n']) - Beta_n(p['V']) * p['n']
@@ -90,8 +110,8 @@ n_0 = alpha_n(V_0) / (alpha_n(V_0) + Beta_n(V_0))
 m_0 = alpha_m(V_0) / (alpha_m(V_0) + Beta_m(V_0))
 h_0 = alpha_h(V_0) / (alpha_h(V_0) + Beta_h(V_0))
 p0 = (0, {'V': V_0, 'n': n_0, 'm': m_0, 'h': h_0})
-dt = 1.1
-n = int((80) / dt) # t ∈ [0, 80 ms]
+dt = 0.005
+n = int((19) / dt) # t ∈ [0, 80 ms]
 derivatives = {'V': dV_dt, 'n': dn_dt, 'm': dm_dt, 'h': dh_dt}
 euler_mv = EulerEstimatorMultivariable(derivatives)
 points = euler_mv.estimate_points(p0, dt, n)
@@ -103,25 +123,26 @@ print(f"  p0: {p0}")
 figure, (plt1, plt2) = plt.subplots(2, 1)
 tss = [p[0] for p in points]
 sss = [stimulus(p[0]) for p in points]
-Vss = [int(p[1]["V"]) for p in points]
-nss = [int(p[1]["n"]) for p in points]
-mss = [int(p[1]["m"]) for p in points]
-hss = [int(p[1]["h"]) for p in points]
+Vss = [p[1]["V"] for p in points]
+nss = [p[1]["n"] for p in points]
+mss = [p[1]["m"] for p in points]
+hss = [p[1]["h"] for p in points]
+
 plt1.plot(tss, sss, label="Stimulus")
 plt1.plot(tss, Vss, label="Membrance Potential")
-# plt.title("Hodgkin-Huxley Neuron Action Potential Model -- Problem CH3_7")
-# plt.ylabel("Voltage (mV)")
-# plt.legend()
-# plt.grid(True)
+plt1.legend()
+plt1.grid(True)
+plt1.set_title("Hodgkin-Huxley Neuron Action Potential Model -- CH3_7")
 
 plt2.plot(tss, nss, label="n")
 plt2.plot(tss, mss, label="m")
 plt2.plot(tss, hss, label="h")
-
-plt.xlabel("time (ms)")
-plt.legend()
-plt.grid(True)
+plt2.legend()
+plt2.grid(True)
+plt2.set_xlabel("time (ms)")
 
 plot_filename = "ch3_7_Hodgkin_Huxley.png"
 plt.savefig(plot_filename)
 print(f"  figure {plot_filename} generated")
+print(f"  DEBUG Max V:{max(Vss)} n:{max(nss)} m:{max(mss)} Min h:{min(hss)}")
+
