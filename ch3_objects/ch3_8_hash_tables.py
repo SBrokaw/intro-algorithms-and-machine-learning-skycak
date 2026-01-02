095e1e7c8f860c4653bfade8b284607e9c774c6b
sbrokaw <brok.sea@gmail.com>
2026-01-02 14:43:57 -0800

    Finished hash tables.

--- ch3_objects/ch3_8_hash_tables.py
+++ ch3_objects/ch3_8_hash_tables.py
@@ -25,6 +25,37 @@ def find(array, key):
 
     return None
 
+class HashTable():
+    def __init__(self, num_buckets):
+        self.ht = [[] for _ in range(num_buckets)]
+
+    @property
+    def num_buckets(self):
+        return len(self.ht)
+
+    def buckets(self):
+        print('[')
+        for bucket in self.ht:
+            print(f"  {bucket},")
+        print(']')
+
+    def hash(self, string):
+        bucket = sum([ord(c) - 97 for c in string.lower()]) % self.num_buckets
+        return bucket
+
+    def insert(self, key, value):
+        self.ht[self.hash(key)].append((key, value))
+        return 0
+
+    def find(self, key):
+        bucket = self.hash(key)
+        for item in self.ht[bucket]:
+            if item[0] == key: 
+                return item[1]
+
+        return None
+
+
 
 array = [[], [], [], [], []] # 5 buckets
 print(array)
@@ -51,3 +82,21 @@ for i, char in enumerate(alphabet):
     output_value = find(array, key)
     desired_value = [i, i**2, i**3]
     assert output_value == desired_value
+
+
+ht = HashTable(3)
+ht.buckets()
+print(f"ht.hash\(\'cabbage\'\) = {ht.hash('cabbage')}")
+ht.insert('cabbage', 5)
+ht.buckets()
+ht.insert('cab', 20)
+ht.buckets()
+ht.insert('c', 17)
+ht.buckets()
+ht.insert('ac', 21)
+ht.buckets()
+print(ht.find('cabbage'))
+print(ht.find('cab'))
+print(ht.find('c'))
+print(ht.find('ac'))
+
